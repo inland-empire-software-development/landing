c38505dd3b90ffb3e35e8982d2f54269bbdcd8c8
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-03-30 12:13:19 -0700

    convert website to react

---
+++ src/App/components/Nav/Nav.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import logo from './assets/iesd-text-white-nobg.svg';
+
+const Nav = () => {
+	return (
+		<div id="nav" className="container-fluid bg-black">
+		  <div className="container">
+		    <div className="row">
+	        <nav className="py-4 w-100 navbar navbar-expand-lg d-flex align-items-center">
+	          <img src={logo} alt="freeCodeCamp logo" />
+	          <button className="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarNav"
+	                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	            <span className="navbar-toggler-icon"></span>
+	          </button>
+	          <div className="w-100 collapse navbar-collapse justify-content-end" id="navbarNav">
+	            <ul className="navbar-nav align-items-center">
+	              <li className="nav-item">
+	                <a className="text-white font-weight-bold" href="#aboutus-div">Overview</a>
+	              </li>
+	              <li className="nav-item">
+	                <a className="text-white font-weight-bold" href="#mission-div">Mission</a>
+	              </li>
+	              <li className="nav-item">
+	                <a className="text-white font-weight-bold" href="#events-div">Events</a>
+	              </li>
+	            </ul>
+	          </div>
+	        </nav>
+	      </div>
+	    </div>
+		</div>
+	);
+};
+
+export default Nav;
---
+++ src/App/components/Sections/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<div id="header-div" className="container">
+		  <header className="header">
+		    <div className="text-box">
+		      <h1 className="heading-primary">
+		        <span className="heading-primary-main">Inland Empire</span>
+		        <span className="heading-primary-main">Software Development</span>
+		        <span className="heading-primary-sub">Riverside, CA</span>
+		      </h1>
+		    </div>
+		  </header>
+		</div>
+	);
+};
+
+export default Header;
